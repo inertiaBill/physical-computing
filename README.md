0f59209e5437e4b532f447c7eb0018a40391f27a
Inertia Bill <inertiaBill@users.noreply.github.com>
2020-06-17 23:31:42 -0400

    Add temperature sensing using ds18b20
    - Collect temperature from two ds18b20 sensors.
    - Get specifics such spreadsheet and sensor device from enviornment
      variables.
    - Refactor and add some methods for displaying spreadsheet data.

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # physical-computing
 Computer interaction with the real world
+
+Temperature monitor (sheet_access.py)
+
+Inserts current temperations in a Google Sheet. The sheet and sensor indentifiers are specified using environment variables.
+
+https://docs.google.com/spreadsheets/d/{spreadsheet_id}/edit#gid=0
+
+```
+export SOLAR_SPREADSHEET={spreadsheet_id_from_URL}
+export SOLAR_SHEET={label_of_sheet_tab}
+export SOLAR_RANGE=A2:E
+export THERMAL_PROB=/sys/bus/w1/devices/28-000007bb1844/w1_slave
+export THERMAL_SENSOR=/sys/bus/w1/devices/28-000007ba00af/w1_slave
+```
+
+If you see,
+
+```
+google.auth.exceptions.RefreshError: ('invalid_grant: Token has been expired or revoked.', '{\n  "error": "invalid_grant",\n  "error_description": "Token has been expired or revoked."\n}')
+```
+
+Remove token.pickle 
--- scripts/sheet_access.py
+++ scripts/sheet_access.py
@@ -1,19 +1,50 @@
 from __future__ import print_function
+from time import sleep
+#from gpiozero import MCP3008
 import pickle
 import os.path
 import datetime
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
+from pprint import pprint
 
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
 # The ID and range of a sample spreadsheet.
 # https://docs.google.com/spreadsheets/d/1zZa5Oo7x5hI3Ys9hd9jTlGvE50XQVeouMpM2UcKfnBw/view#gid=0
-SOLAR_OVEN_SPREADSHEET_ID = '1zZa5Oo7x5hI3Ys9hd9jTlGvE50XQVeouMpM2UcKfnBw'
-SOLAR_OVEN_RANGE_NAME = 'Dev!A2:C'
-SOLAR_OVEN_SHEET_NAME = 'Dev'
+SOLAR_SPREADSHEET_ID = os.environ['SOLAR_SPREADSHEET']
+SOLAR_RANGE_NAME = os.environ['SOLAR_SHEET'] + '!' + os.environ['SOLAR_RANGE']
+
+# TODO: Figure out why GPIO Zero cannot be found.
+#
+# PHOTO_SHADE = MCP3008(channel=3)
+# PHOTO_SUN = MCP3008(channel=6)
+# 
+# pot = MCP3008(channel=0)
+# therm = MCP3008(channel=2)
+# lm35 = MCP3008(channel=4)
+# therm2 = MCP3008(channel=5)
+
+THERM_OVEN = os.environ['THERMAL_PROB']
+THERM_AIR = os.environ['THERMAL_SENSOR']
+
+def get_temp(sensor_file):
+    found_data = False
+    
+    while found_data == False:
+        with open(sensor_file, 'r') as sense_data:
+            for line in sense_data:
+                if line.strip()[-3:] == 'YES':
+                    found_data = True
+                data_location = line.find('t=')
+                if data_location != -1:
+                    temp = float(line[data_location+2:]) / 1000.0
+                    found_data = True
+        sleep(0.2)
+    print('Temp: {}'.format(temp))
+    return temp    
 
 def append_current_temps(service):
     """Append current temperatures to spreadsheet.
@@ -26,7 +57,7 @@ def append_current_temps(service):
     print('Timestamp: {}'.format(time_string))
     values = [
         [
-            time_string, 44, 19
+            time_string, get_temp(THERM_OVEN), get_temp(THERM_AIR)
         ]
     ]
     body = {
@@ -35,18 +66,59 @@ def append_current_temps(service):
 
     # valueInputOption - USER_ENTERED
     result = service.spreadsheets().values().append(
-        spreadsheetId=SOLAR_OVEN_SPREADSHEET_ID, range=SOLAR_OVEN_RANGE_NAME,
+        spreadsheetId=SOLAR_SPREADSHEET_ID, range=SOLAR_RANGE_NAME,
         valueInputOption="USER_ENTERED", body=body).execute()
     print('Debug: {} cells appended.'.format(result \
                                         .get('updates') \
-                                        .get('updatedCells')))                                        
+                                        .get('updatedCells')))
+    
+def print_sheet_info(service):
+    request = service.spreadsheets().get(spreadsheetId=SOLAR_SPREADSHEET_ID,
+                                         ranges=None,
+                                         includeGridData=None)
+    values = request.execute()
+    pprint(values)
 
+def print_last_row(service):
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SOLAR_SPREADSHEET_ID,
+                                range=SOLAR_RANGE_NAME).execute()
+    values = result.get('values', [])
+    print('Found {} rows.'.format(len(values)))
+    print('Last row: {}, {}, {}'.format(values[-1][0], values[-1][1],
+                                        values[-1][2]))
+    print('')
+
+def print_rows(service):
+    # Call the Sheets API
+    sheet = service.spreadsheets()
+    result = sheet.values().get(spreadsheetId=SOLAR_SPREADSHEET_ID,
+                                range=SOLAR_RANGE_NAME).execute()
+    values = result.get('values', [])
+
+    # Output spreadsheet data
+    # TODO: Can we output just last few rows?
+    # TODO: Should we output everytime?
+    if not values:
+        print('Warning: No data found')
+    else:
+        print('Time, Inside, Outside')
+        for row in values:
+            # Print columns A through C, which correspond to indices 0, 1, 2.
+            print('{}, {}, {}'.format(row[0], row[1], row[2]))
+    
 def main():
     """Writes and reads using Sheets API.
     Prints values from specified spreadsheet.
     """
 
     creds = None
+    
+    print('SOLAR_SPREADSHEET_ID: {}'.format(SOLAR_SPREADSHEET_ID))
+    print('SOLAR_RANGE_NAME: {}'.format(SOLAR_RANGE_NAME))
+    print('THERM_OVEN: {}'.format(THERM_OVEN))
+    print('THERM_AIR: {}'.format(THERM_AIR))
 
     # The file token.pickle stores the user's access and refresh tokens, and is
     # created automatically when the authorization flow completes for the first
@@ -72,22 +144,13 @@ def main():
     # Add current values to sheet
     append_current_temps(service)
 
-    # Call the Sheets API
-    sheet = service.spreadsheets()
-    result = sheet.values().get(spreadsheetId=SOLAR_OVEN_SPREADSHEET_ID,
-                                range=SOLAR_OVEN_RANGE_NAME).execute()
-    values = result.get('values', [])
+    #print_rows(service)
 
-    # Output spreadsheet data
-    # TODO: Can we output just last few rows?
-    # TODO: Should we output everytime?
-    if not values:
-        print('Warning: No data found')
-    else:
-        print('Time, Inside, Outside')
-        for row in values:
-            # Print columns A through C, which correspond to indices 0, 1, 2.
-            print('{}, {}, {}'.format(row[0], row[1], row[2]))
+#     print_last_row(service)
+#     print('Photo shade: {}'.format(PHOTO_SHADE.value))
+#     print('Photo sun: {}'.format(PHOTO_SUN.value))
+
+    #print_sheet_info(service)
 
 if __name__ == '__main__':
     main()
